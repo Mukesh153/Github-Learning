43cf19ec29545621b4c32d4ffb28b230a9619aca
Mukesh <mukesh.tiwari@rigvedtech.com>
2019-02-07 18:04:28 +0530

    Basic Login Form

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { FormsModule } from '@angular/forms';
+import { User1Component } from './user1/user1.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserComponent,
+    User1Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
