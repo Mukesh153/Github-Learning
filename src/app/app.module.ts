ad39f49db502be41c35e175f274dcb2dd042e1e1
Mukesh Tiwari <mukeshrahat@gmail.com>
2019-02-10 14:48:36 +0530

    Application Login Form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {MatButtonModule, MatCheckboxModule} from '@angular/material'
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
 import { FormsModule } from '@angular/forms';
 import { User1Component } from './user1/user1.component';
+
+
 @NgModule({
   declarations: [
     AppComponent,
